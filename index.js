6ca6e5cc637a4b89dd13df44d68571a3e33630cb
Kam Lasater <kam@cyclic.sh>
2021-08-16 11:17:35 -0400

    Added slack webhook call

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+require('dotenv').config()
+
+// Init the slack webhook
+const { IncomingWebhook } = require('@slack/webhook');
+const url = process.env.SLACK_WEBHOOK_URL;
+const webhook = new IncomingWebhook(url);
+
 /**
- * This is the main entrypoint to your Probot app
+ * This is the main entrypoint for the Probot app
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
   // Your code here
-  app.log.info("Yay, the app was loaded!");
+  app.log.info("App was loaded");
 
   app.on("issues.opened", async (context) => {
+
+    console.log(JSON.stringify(context, null, 2))
+
+    // Send the notification
+    (async () => {
+      await webhook.send({
+        text: 'An issue has been opened.',
+      });
+    })();
+
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
