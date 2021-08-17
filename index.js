f2039d0bfb7fae56258e0880ad0927e494f78816
Kam Lasater <kam@cyclic.sh>
2021-08-17 10:18:32 -0400

    Cleanup excess comments

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 require('dotenv').config()
 
-// Init the slack webhook
 const { IncomingWebhook } = require('@slack/webhook');
 const url = process.env.SLACK_WEBHOOK_URL;
 const webhook = new IncomingWebhook(url);
 
-/**
- * This is the main entrypoint for the Probot app
- * @param {import('probot').Probot} app
- */
 module.exports = (app) => {
-  // Your code here
   app.log.info("App was loaded");
 
   app.on("issues.opened", async (context) => {
 
     console.log(JSON.stringify(context, null, 2))
 
-    // Send the notification
     await webhook.send({
         text: `Issue opened: ${context.payload.issue.html_url}`,
     });
@@ -27,10 +20,4 @@ module.exports = (app) => {
     });
     return context.octokit.issues.createComment(issueComment);
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
